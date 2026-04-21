6a9acc4e464324173d7f601b5879305eb3a9e72e
automationzone365 <automationzone365@gmail.com>
2026-04-21 11:30:50 +0530

    Day 20 - Lombok integration completed

--- src/test/java/models/User.java
+++ src/test/java/models/User.java
@@ -1,45 +1,13 @@
 package models;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
     private Integer id;
     private String name;
     private String username;
     private String email;
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-
 }
